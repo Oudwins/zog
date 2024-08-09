fc1489a688d75da5652d5cb39dcc5ff3d843c032
oudwins <tm@tristanmayo.com>
2024-08-09 19:56:49 +0200

    feat: zhttp & zenv v1

--- zenv/zenv.go
+++ zenv/zenv.go
@@ -13,7 +13,10 @@ type envDataProvider struct {
 
 func (e *envDataProvider) Get(key string) any {
 	return os.Getenv(key)
+}
 
+func (e *envDataProvider) GetNestedProvider(key string) p.DataProvider {
+	return e
 }
 
 // Parses environment variables into destinationStruct
---
+++ zhttp/zhttp.go
@@ -0,0 +1,34 @@
+package zhttp
+
+import (
+	"net/http"
+	"net/url"
+
+	p "github.com/Oudwins/zog/primitives"
+)
+
+type urlDataProvider struct {
+	Data url.Values
+}
+
+var _ p.DataProvider = urlDataProvider{}
+
+func (u urlDataProvider) Get(key string) any {
+	if len(u.Data[key]) > 1 {
+		return u.Data[key]
+	} else {
+		return u.Data.Get(key)
+	}
+}
+
+func (u urlDataProvider) GetNestedProvider(key string) p.DataProvider {
+	return u
+}
+
+func NewRequestDataProvider(r *http.Request) (urlDataProvider, error) {
+	err := r.ParseForm()
+	if err != nil {
+		return urlDataProvider{}, err
+	}
+	return urlDataProvider{Data: r.Form}, nil
+}
