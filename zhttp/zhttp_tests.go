ed68762ce2cc8e08aec39844e5ee47981de3a113
oudwins <tm@tristanmayo.com>
2024-08-16 12:53:45 +0200

    test: http tests

--- zhttp/zhttp_tests.go
+++ zhttp/zhttp_tests.go
@@ -1,101 +1,102 @@
 package zhttp
 
-// import (
-// 	"net/http"
-// 	"strings"
-// 	"testing"
+import (
+	"net/http"
+	"strings"
+	"testing"
 
-// 	p "github.com/Oudwins/zog/primitives"
-// 	"github.com/stretchr/testify/assert"
-// )
+	z "github.com/Oudwins/zog"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestRequest(t *testing.T) {
-// 	formData := "name=JohnDoe&email=john@doe.com&age=30&isMarried=true&lights=on&cash=10.5&swagger=doweird"
+func TestRequest(t *testing.T) {
+	formData := "name=JohnDoe&email=john@doe.com&age=30&isMarried=true&lights=on&cash=10.5&swagger=doweird"
 
-// 	// Create a fake HTTP request with form data
-// 	req, err := http.NewRequest("POST", "/submit?foo=bar&bar=foo&foo=baz", strings.NewReader(formData))
-// 	if err != nil {
-// 		t.Fatalf("Error creating request: %v", err)
-// 	}
-// 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-// 	type User struct {
-// 		Email     string  `form:"email"`
-// 		Name      string  `form:"name"`
-// 		Age       int     `form:"age"`
-// 		IsMarried bool    `form:"isMarried"`
-// 		Lights    bool    `form:"lights"`
-// 		Cash      float64 `form:"cash"`
-// 		Swagger   string  `form:"swagger"`
-// 	}
-// 	schema := Schema{
-// 		"email":     String().Email(),
-// 		"name":      String().Min(3).Max(10),
-// 		"age":       Int().GT(18),
-// 		"isMarried": Bool().True(),
-// 		"lights":    Bool().True(),
-// 		"cash":      Float().GT(10.0),
-// 		"swagger": String().Refine("swagger", "should be doweird", func(rule p.Rule) bool {
-// 			return rule.FieldValue.(string) == "doweird"
-// 		}),
-// 	}
-// 	u := User{}
+	// Create a fake HTTP request with form data
+	req, err := http.NewRequest("POST", "/submit", strings.NewReader(formData))
+	if err != nil {
+		t.Fatalf("Error creating request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	type User struct {
+		Email     string  `zog:"email"`
+		Name      string  `zog:"name"`
+		Age       int     `zog:"age"`
+		IsMarried bool    `zog:"isMarried"`
+		Lights    bool    `zog:"lights"`
+		Cash      float64 `zog:"cash"`
+		Swagger   string  `zog:"swagger"`
+	}
+	schema := z.Struct(z.Schema{
+		"email":     z.String().Email(),
+		"name":      z.String().Min(3).Max(10),
+		"age":       z.Int().GT(18),
+		"isMarried": z.Bool().True(),
+		"lights":    z.Bool().True(),
+		"cash":      z.Float().GT(10.0),
+		"swagger": z.String().Test("swagger", z.Message("should be doweird"), func(val any, ctx *z.ParseCtx) bool {
+			return val.(string) == "doweird"
+		}),
+	})
+	u := User{}
 
-// 	errs, ok := Request(req, &u, schema)
+	dp, err := NewRequestDataProvider(req)
+	assert.Nil(t, err)
+	errs := schema.Parse(dp, &u)
 
-// 	assert.Equal(t, "john@doe.com", u.Email)
-// 	assert.Equal(t, "JohnDoe", u.Name)
-// 	assert.Equal(t, 30, u.Age)
-// 	assert.True(t, u.IsMarried)
-// 	assert.True(t, u.Lights)
-// 	assert.Equal(t, 10.5, u.Cash)
-// 	assert.Equal(t, u.Swagger, "doweird")
-// 	assert.Empty(t, errs)
-// 	assert.True(t, ok)
-// }
+	assert.Equal(t, "john@doe.com", u.Email)
+	assert.Equal(t, "JohnDoe", u.Name)
+	assert.Equal(t, 30, u.Age)
+	assert.True(t, u.IsMarried)
+	assert.True(t, u.Lights)
+	assert.Equal(t, 10.5, u.Cash)
+	assert.Equal(t, u.Swagger, "doweird")
+	assert.Empty(t, errs)
+}
 
-// func TestRequestParams(t *testing.T) {
-// 	formData := "name=JohnDoe&email=john@doe.com&age=30&age=20&isMarried=true&lights=on&cash=10.5&swagger=doweird&swagger=swagger"
+func TestRequestParams(t *testing.T) {
+	formData := "name=JohnDoe&email=john@doe.com&age=30&age=20&isMarried=true&lights=on&cash=10.5&swagger=doweird&swagger=swagger"
 
-// 	// Create a fake HTTP request with form data
-// 	req, err := http.NewRequest("POST", "/submit?"+formData, nil)
-// 	if err != nil {
-// 		t.Fatalf("Error creating request: %v", err)
-// 	}
+	// Create a fake HTTP request with form data
+	req, err := http.NewRequest("POST", "/submit?"+formData, nil)
+	if err != nil {
+		t.Fatalf("Error creating request: %v", err)
+	}
 
-// 	type User struct {
-// 		Email     string   `param:"email"`
-// 		Name      string   `param:"name"`
-// 		Age       int      `param:"age"`
-// 		IsMarried bool     `param:"isMarried"`
-// 		Lights    bool     `param:"lights"`
-// 		Cash      float64  `param:"cash"`
-// 		Swagger   []string `param:"swagger"`
-// 	}
+	type User struct {
+		Email     string   `param:"email"`
+		Name      string   `param:"name"`
+		Age       int      `param:"age"`
+		IsMarried bool     `param:"isMarried"`
+		Lights    bool     `param:"lights"`
+		Cash      float64  `param:"cash"`
+		Swagger   []string `param:"swagger"`
+	}
 
-// 	schema := Schema{
-// 		"email":     String().Email(),
-// 		"name":      String().Min(3).Max(10),
-// 		"age":       Int().GT(18),
-// 		"isMarried": Bool().True(),
-// 		"lights":    Bool().True(),
-// 		"cash":      Float().GT(10.0),
-// 		"swagger": Slice(
-// 			String().Min(1)).Min(2),
-// 	}
-// 	u := User{}
+	schema := z.Struct(z.Schema{
+		"email":     z.String().Email(),
+		"name":      z.String().Min(3).Max(10),
+		"age":       z.Int().GT(18),
+		"isMarried": z.Bool().True(),
+		"lights":    z.Bool().True(),
+		"cash":      z.Float().GT(10.0),
+		"swagger": z.Slice(
+			z.String().Min(1)).Min(2),
+	})
+	u := User{}
+	dp, err := NewRequestDataProvider(req)
+	assert.Nil(t, err)
+	errs := schema.Parse(dp, &u)
 
-// 	errs, ok := RequestParams(req, &u, schema)
-
-// 	assert.Equal(t, "john@doe.com", u.Email)
-// 	assert.Equal(t, "JohnDoe", u.Name)
-// 	assert.Equal(t, 30, u.Age)
-// 	assert.True(t, u.IsMarried)
-// 	assert.True(t, u.Lights)
-// 	assert.Equal(t, 10.5, u.Cash)
-// 	assert.Equal(t, u.Swagger, []string{"doweird", "swagger"})
-// 	assert.Empty(t, errs)
-// 	assert.True(t, ok)
-// }
+	assert.Equal(t, "john@doe.com", u.Email)
+	assert.Equal(t, "JohnDoe", u.Name)
+	assert.Equal(t, 30, u.Age)
+	assert.True(t, u.IsMarried)
+	assert.True(t, u.Lights)
+	assert.Equal(t, 10.5, u.Cash)
+	assert.Equal(t, u.Swagger, []string{"doweird", "swagger"})
+	assert.Empty(t, errs)
+}
 
 // func TestStringURL(t *testing.T) {
 // 	type Foo struct {
