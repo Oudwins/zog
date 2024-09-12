a4e15e42ad9f8a76ff8b199283e51802006d9768
oudwins <tm@tristanmayo.com>
2024-09-12 19:20:26 +0200

    test: improved tests for zenv

--- zenv/zenv_test.go
+++ zenv/zenv_test.go
@@ -10,16 +10,19 @@ import (
 
 func TestEnvParsing(t *testing.T) {
 	type TestStruct struct {
-		Str string `zog:"TEST_STR"`
+		Str  string `zog:"TEST_STR"`
+		PORT int
 	}
 	env := TestStruct{}
 	schema := z.Struct(z.Schema{
-		"str": z.String().Required(),
+		"str":  z.String().Required(),
+		"pORT": z.Int().Default(8080),
 	})
 
 	os.Setenv("TEST_STR", "hello")
 	err := schema.Parse(NewDataProvider(), &env)
 	assert.Nil(t, err)
-	assert.Equal(t, env.Str, "hello")
+	assert.Equal(t, "hello", env.Str)
+	assert.Equal(t, 8080, env.PORT)
 	os.Setenv("TEST_STR", "")
 }
