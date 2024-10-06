75a9b2b53b6e2bc8ceb37a5dd077d064552cd3d4
oudwins <tm@tristanmayo.com>
2024-10-06 10:18:11 +0200

    refactor: renamed is zero value struct

--- boolean.go
+++ boolean.go
@@ -34,7 +34,7 @@ func (v *boolProcessor) Parse(data any, dest *bool, options ...ParsingOption) p.
 }
 
 func (v *boolProcessor) process(val any, dest any, path p.PathBuilder, ctx ParseCtx) {
-	primitiveProcessor(val, dest, path, ctx, v.preTransforms, v.tests, v.postTransforms, v.defaultVal, v.required, v.catch, conf.Coercers.Bool, conf.IsZeroValue.Bool)
+	primitiveProcessor(val, dest, path, ctx, v.preTransforms, v.tests, v.postTransforms, v.defaultVal, v.required, v.catch, conf.Coercers.Bool, conf.ParseIsZeroValue.Bool)
 }
 
 // GLOBAL METHODS
