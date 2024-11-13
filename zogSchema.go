9698f31e09e7e12a62af5b0aa218a8a4ab2fd8c2
oudwins <tm@tristanmayo.com>
2024-11-13 19:28:33 +0100

    refactor: moved generic parser function to zogSchema file

--- zogSchema.go
+++ zogSchema.go
@@ -1,6 +1,10 @@
 package zog
 
 import (
+	"log"
+	"reflect"
+	"time"
+
 	"github.com/Oudwins/zog/conf"
 	p "github.com/Oudwins/zog/internals"
 	"github.com/Oudwins/zog/zconst"
@@ -31,3 +35,121 @@ type ZogErrMap = p.ZogErrMap
 
 // Test is the test object
 type Test = p.Test
+
+// ! PRIMITIVE PROCESSING
+
+func getDestType(dest any) zconst.ZogType {
+	switch reflect.TypeOf(dest).Kind() {
+	case reflect.Slice:
+		return zconst.TypeSlice
+	case reflect.Struct:
+		if reflect.TypeOf(dest) == reflect.TypeOf(time.Time{}) {
+			return zconst.TypeTime
+		}
+		return zconst.TypeStruct
+	case reflect.Float64:
+	case reflect.Int:
+		return zconst.TypeNumber
+	case reflect.Bool:
+		return zconst.TypeBool
+	case reflect.String:
+		return zconst.TypeString
+	default:
+		log.Fatal("Unsupported destination type")
+	}
+	// should never get here
+	return zconst.TypeString
+}
+
+func primitiveProcessor[T p.ZogPrimitive](val any, dest any, path p.PathBuilder, ctx ParseCtx, preTransforms []p.PreTransform, tests []p.Test, postTransforms []p.PostTransform, defaultVal *T, required *p.Test, catch *T, coercer conf.CoercerFunc, isZeroFunc p.IsZeroValueFunc) {
+	canCatch := catch != nil
+	hasCatched := false
+
+	destPtr := dest.(*T)
+	destType := getDestType(*destPtr)
+	// 1. preTransforms
+	for _, fn := range preTransforms {
+		nVal, err := fn(val, ctx)
+		// bail if error in preTransform
+		if err != nil {
+			if canCatch {
+				*destPtr = *catch
+				hasCatched = true
+				break
+			}
+			ctx.NewError(path, Errors.WrapUnknown(val, destType, err))
+			return
+		}
+		val = nVal
+	}
+	// 4. postTransforms
+	defer func() {
+		// only run posttransforms on success
+		if !ctx.HasErrored() {
+			for _, fn := range postTransforms {
+				err := fn(destPtr, ctx)
+				if err != nil {
+					ctx.NewError(path, Errors.WrapUnknown(val, destType, err))
+					return
+				}
+			}
+		}
+	}()
+
+	if hasCatched {
+		return
+	}
+
+	// 2. cast data to string & handle default/required
+	isZeroVal := isZeroFunc(val, ctx)
+
+	if isZeroVal {
+		if defaultVal != nil {
+			*destPtr = *defaultVal
+		} else if required == nil {
+			// This handles optional case
+			return
+		} else {
+			// is required & zero value
+			// required
+			if catch != nil {
+				*destPtr = *catch
+				hasCatched = true
+			} else {
+				ctx.NewError(path, Errors.FromTest(val, destType, required, ctx))
+				return
+			}
+		}
+	} else {
+		newVal, err := coercer(val)
+		if err == nil {
+			*destPtr = newVal.(T)
+		} else {
+			if canCatch {
+				*destPtr = *catch
+				hasCatched = true
+			} else {
+				ctx.NewError(path, Errors.New(zconst.ErrCodeCoerce, val, destType, nil, "", err))
+				return
+			}
+		}
+	}
+
+	if hasCatched {
+		return
+	}
+	// 3. tests
+	for _, test := range tests {
+		if !test.ValidateFunc(*destPtr, ctx) {
+			// catching the first error if catch is set
+			if catch != nil {
+				*destPtr = *catch
+				hasCatched = true //nolint
+				break
+			}
+			ctx.NewError(path, Errors.FromTest(val, destType, &test, ctx))
+		}
+	}
+
+	// 4. postTransforms -> Done above on defer
+}
