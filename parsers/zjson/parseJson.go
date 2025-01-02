2365ce2bedf74b65bb2e84a340ba4aed12798c42
oudwins <tm@tristanmayo.com>
2025-01-02 14:09:14 +0100

    fix: close closer if available

--- parsers/zjson/parseJson.go
+++ parsers/zjson/parseJson.go
@@ -9,19 +9,19 @@ import (
 	"github.com/Oudwins/zog/zconst"
 )
 
-func Unmarshal(data []byte) p.DpFactory {
-	return func() (p.DataProvider, p.ZogError) {
-		var m map[string]any
-		err := json.Unmarshal(data, &m)
-		if err != nil {
-			return nil, &p.ZogErr{C: zconst.ErrCodeInvalidJSON, Err: err}
-		}
-		if m == nil {
-			return nil, &p.ZogErr{C: zconst.ErrCodeInvalidJSON, Err: errors.New("nill json body")}
-		}
-		return p.NewMapDataProvider(m), nil
-	}
-}
+// func Unmarshal(data []byte) p.DpFactory {
+// 	return func() (p.DataProvider, p.ZogError) {
+// 		var m map[string]any
+// 		err := json.Unmarshal(data, &m)
+// 		if err != nil {
+// 			return nil, &p.ZogErr{C: zconst.ErrCodeInvalidJSON, Err: err}
+// 		}
+// 		if m == nil {
+// 			return nil, &p.ZogErr{C: zconst.ErrCodeInvalidJSON, Err: errors.New("nill json body")}
+// 		}
+// 		return p.NewMapDataProvider(m), nil
+// 	}
+// }
 
 // Decodes JSON data. Does not support json arrays or primitives
 /*
@@ -34,6 +34,10 @@ func Unmarshal(data []byte) p.DpFactory {
 */
 func Decode(r io.Reader) p.DpFactory {
 	return func() (p.DataProvider, p.ZogError) {
+		closer, ok := r.(io.Closer)
+		if ok {
+			defer closer.Close()
+		}
 		var m map[string]any
 		decod := json.NewDecoder(r)
 		err := decod.Decode(&m)
