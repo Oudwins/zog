11a48935ef7602105d2b9445269b1b005692d735
Gabriel <45315755+gm0stache@users.noreply.github.com>
2025-02-08 23:48:05 +0100

    refactor: use template for output

---
+++ cmd/ssg/some_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+//go:generate go run main.go
+type MyType struct {
+	Field1 string
+	Field2 int
+}
+
+// type MyOtherType struct {
+//     Field1 string
+//     Field2 int
+// }
