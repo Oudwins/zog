9b42c99381f6b5059f87df4676266361e4295cc7
Gabriel <45315755+gm0stache@users.noreply.github.com>
2025-02-09 13:59:33 +0100

    feat: convert field types to 'zog.ZogSchema'

--- cmd/ssg/some_test.go
+++ cmd/ssg/some_test.go
@@ -1,11 +1,15 @@
 package main
 
-//go:generate go run main.go ssg -output=./schema/generated.go
+// example with most basic usage
+//
+//go:generate go run main.go ssg
 type MyType struct {
 	Field1 string `json:"field1"`
 	Field2 int
 }
 
+// example with custom output path
+//
 //go:generate go run main.go ssg -output=./schema/generated.go
 type MyOtherType struct {
 	Field1 string
