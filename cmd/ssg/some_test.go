af9e75c9c03c8f2c116acd38fdc887d57efad1e5
Gabriel <45315755+gm0stache@users.noreply.github.com>
2025-02-09 13:50:08 +0100

    feat: enable custom path dumping

--- cmd/ssg/some_test.go
+++ cmd/ssg/some_test.go
@@ -1,12 +1,13 @@
 package main
 
-//go:generate go run main.go
+//go:generate go run main.go ssg -output=./schema/generated.go
 type MyType struct {
 	Field1 string `json:"field1"`
 	Field2 int
 }
 
-// type MyOtherType struct {
-//     Field1 string
-//     Field2 int
-// }
+//go:generate go run main.go ssg -output=./schema/generated.go
+type MyOtherType struct {
+	Field1 string
+	Field2 int
+}
