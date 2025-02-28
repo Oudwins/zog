14f3511bb2a18750d429133c77504c05e09d2777
oudwins <tm@tristanmayo.com>
2025-02-28 12:02:24 +0100

    perf: removed one allocation per schema

--- internals/zeroValues.go
+++ internals/zeroValues.go
@@ -9,18 +9,8 @@ type IsZeroValueFunc = func(val any, ctx Ctx) bool
 
 // checks that the value is the zero value for its type
 func IsZeroValue(x any) bool {
-	if x == nil {
-		return true
-	}
-
 	v := reflect.ValueOf(x)
-	if !v.IsValid() {
-		return true
-	}
-
-	// Check if the value is the zero value for its type
-	zeroValue := reflect.Zero(v.Type())
-	return reflect.DeepEqual(v.Interface(), zeroValue.Interface())
+	return !v.IsValid() || v.IsZero()
 }
 
 // checks if the value is the zero value but only for parsing purposes (i.e the parse function)
