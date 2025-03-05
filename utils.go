a780fc2f6d2b2f908bdc4f3541eb28d523af4334
tristan <tm@tristanmayo.com>
2025-03-05 08:54:41 +0100

    docs: godoc for issues utils

--- utils.go
+++ utils.go
@@ -86,6 +86,7 @@ type issueHelpers struct {
 
 var Issues = issueHelpers{}
 
+// SanitizeMap returns a map of issue messages for each key in the map. It keeps only the issue messages and strips out any other issue data.
 func (i *issueHelpers) SanitizeMap(m ZogIssueMap) map[string][]string {
 	errs := make(map[string][]string, len(m))
 	for k, v := range m {
@@ -94,12 +95,14 @@ func (i *issueHelpers) SanitizeMap(m ZogIssueMap) map[string][]string {
 	return errs
 }
 
+// Suger function that does both sanitize and collect.
 func (i issueHelpers) SanitizeMapAndCollect(m ZogIssueMap) map[string][]string {
 	errs := i.SanitizeMap(m)
 	i.CollectMap(m)
 	return errs
 }
 
+// SanitizeList returns a slice of issue messages for each issue in the list. It keeps only the issue messages and strips out any other issue data.
 func (i *issueHelpers) SanitizeList(l ZogIssueList) []string {
 	errs := make([]string, len(l))
 	for i, err := range l {
@@ -108,24 +111,28 @@ func (i *issueHelpers) SanitizeList(l ZogIssueList) []string {
 	return errs
 }
 
-func (i *issueHelpers) SanitizeListAndCollect(l ZogIssueList) []string {
+// Suger function that does both sanitize and collect.
+func (i issueHelpers) SanitizeListAndCollect(l ZogIssueList) []string {
 	errs := i.SanitizeList(l)
 	i.CollectList(l)
 	return errs
 }
 
+// Collects a ZogIssueMap to be reused by Zog. This will "free" the issues in the map. This can help make Zog more performant by reusing issue structs.
 func (i *issueHelpers) CollectMap(issues ZogIssueMap) {
 	for _, list := range issues {
 		i.CollectList(list)
 	}
 }
 
+// Collects a ZogIssueList to be reused by Zog. This will "free" the issues in the list. This can help make Zog more performant by reusing issue structs.
 func (i *issueHelpers) CollectList(issues ZogIssueList) {
 	for _, iss := range issues {
 		i.Collect(iss)
 	}
 }
 
+// Collects a ZogIssue to be reused by Zog. This will "free" the issue. This can help make Zog more performant by reusing issue structs.
 func (i *issueHelpers) Collect(issue *ZogIssue) {
 	p.FreeIssue(issue)
 }
