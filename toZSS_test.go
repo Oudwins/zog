41990c2dd6c821fc9d5c66fcd69944d5822668ba
oudwins <tm@tristanmayo.com>
2026-01-04 17:55:32 +0100

    wp

--- toZSS_test.go
+++ toZSS_test.go
@@ -12,15 +12,63 @@ func normalize(s string) string {
 	return strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(s, " ", ""), "\n", ""), "\t", "")
 }
 
+func baseZSSJson(schema string) string {
+	return `{
+		"version": "` + string(zss.ZSS_VERSION_LATEST) + `",
+		"schema": ` + schema + `
+	}`
+}
+
 func TestToJsonString(t *testing.T) {
 	s := String().Required().Default("Testing!").Catch("Testing2!").Min(1)
 	serialized, err := EXPERIMENTAL_TO_ZSS(s)
 	assert.Nil(t, err)
 	assert.NotNil(t, serialized)
 
-	expected := `{
-		"version": "` + string(zss.ZSS_VERSION_LATEST) + `",
-		"schema": {
+	expected := baseZSSJson(`{
+		"kind": "string",
+		"goType": null,
+		"format": null,
+		"processors": [
+			{
+				"kind": "test",
+				"test": {
+					"id": "min",
+					"message": "",
+					"issuePath": null,
+					"params": {
+						"min": 1
+					}
+				},
+				"transformer": null
+			}
+		],
+		"child": null,
+		"required": {
+			"id": "required",
+			"message": "",
+			"issuePath": null,
+			"params": {}
+		},
+		"defaultValue": "Testing!",
+		"catchValue": "Testing2!"
+	}`)
+
+	assert.Equal(t, normalize(expected), normalize(string(serialized)))
+}
+
+func TestToJsonPtr(t *testing.T) {
+	s := Ptr(String().Required().Default("Testing!").Catch("Testing2!").Min(1))
+	serialized, err := EXPERIMENTAL_TO_ZSS(s)
+	assert.Nil(t, err)
+	assert.NotNil(t, serialized)
+
+	expected := baseZSSJson(`{
+		"kind": "ptr",
+		"goType": null,
+		"format": null,
+		"processors": null,
+		"child": {
 			"kind": "string",
 			"goType": null,
 			"format": null,
@@ -47,58 +95,11 @@ func TestToJsonString(t *testing.T) {
 			},
 			"defaultValue": "Testing!",
 			"catchValue": "Testing2!"
-		}
-	}`
-
-	assert.Equal(t, normalize(expected), normalize(string(serialized)))
-}
-
-func TestToJsonPtr(t *testing.T) {
-	s := Ptr(String().Required().Default("Testing!").Catch("Testing2!").Min(1))
-	serialized, err := EXPERIMENTAL_TO_ZSS(s)
-	assert.Nil(t, err)
-	assert.NotNil(t, serialized)
-
-	expected := `{
-		"version": "` + string(zss.ZSS_VERSION_LATEST) + `",
-		"schema": {
-			"kind": "ptr",
-			"goType": null,
-			"format": null,
-			"processors": null,
-			"child": {
-				"kind": "string",
-				"goType": null,
-				"format": null,
-				"processors": [
-					{
-						"kind": "test",
-						"test": {
-							"id": "min",
-							"message": "",
-							"issuePath": null,
-							"params": {
-								"min": 1
-							}
-						},
-						"transformer": null
-					}
-				],
-				"child": null,
-				"required": {
-					"id": "required",
-					"message": "",
-					"issuePath": null,
-					"params": {}
-				},
-				"defaultValue": "Testing!",
-				"catchValue": "Testing2!"
-			},
-			"required": null,
-			"defaultValue": null,
-			"catchValue": null
-		}
-	}`
+		},
+		"required": null,
+		"defaultValue": null,
+		"catchValue": null
+	}`)
 
 	assert.Equal(t, normalize(expected), normalize(string(serialized)))
 }
